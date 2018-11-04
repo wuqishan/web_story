6685d3e8caba833480dbb65cb470b3a75afbcbf1
wuqishan <1174955828@qq.com>
2018-11-04 19:52:31 +0800

    refs

--- app/Helper/PagingHelper.php
+++ app/Helper/PagingHelper.php
@@ -10,21 +10,15 @@ class PagingHelper
     /**
      * @param integer       $total              总数
      * @param integer       $pageNumber         每页显示的条数
-     * @param string        $currentPageField   获取当前页数的键，默认为:page
      * @param integer       $maxPageShow        最大显示页数
      * @return Paginator    $paginator
      */
-    public static function pageInit($total, $pageNumber, $currentPageField = 'page', $maxPageShow = 7)
+    public static function pageInit($total, $pageNumber, $maxPageShow = 7)
     {
-        $currentPage = request()->get($currentPageField, 1);
+        $currentPage = request()->get('page', 1);
         $requestUri = request()->getRequestUri();
-        $urlPattern = preg_replace("/&{$currentPageField}=\d*/", '', $requestUri);
-        $urlPattern = preg_replace("/\?{$currentPageField}=\d*/", '', $urlPattern);
-        if (strpos($urlPattern, '?') === false) {
-            $urlPattern .= '?page=(:num)';
-        } else {
-            $urlPattern .= '&page=(:num)';
-        }
+        $urlPattern = preg_replace("/(\?|&)page=\d*/", '\1page=(:num)', $requestUri);
+
         $paginator = new Paginator($total, $pageNumber, $currentPage, $urlPattern);
         $paginator->setMaxPagesToShow($maxPageShow);
 
