d7a4e95710504ec1234c102439d078dc6b92c1b4
wuqishan <1174955828@qq.com>
2018-11-11 11:29:33 +0800

    refs

--- resources/views/admin/chapter/index.blade.php
+++ resources/views/admin/chapter/index.blade.php
@@ -191,13 +191,7 @@
 
             $('.unique_code').dblclick(function () {
                 var str = $(this).attr('data-title');
-                var save = function(e){
-                    e.clipboardData.setData('text/plain', str);
-                    e.preventDefault();
-                };
-                document.addEventListener('copy', save);
-                document.execCommand('copy');
-                document.removeEventListener('copy',save);
+                $(this).text(str);
             });
 
             // tips显示
