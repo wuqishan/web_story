44a39a9e5ff183a8647c64dc6469ad0189612149
wells <qishan.wu.1@lexisnexis.com>
2018-11-20 10:00:59 +0800

    refs

--- resources/views/admin/common/nav.blade.php
+++ resources/views/admin/common/nav.blade.php
@@ -44,6 +44,7 @@
             <ul class="treeview-menu">
                 <li><a class="treeview-item" data-nav="admin.category.index" href="{{ route('admin.category.index') }}"><i class="icon fa fa-circle-o"></i> 分类列表</a></li>
                 <li><a class="treeview-item" data-nav="admin.author.index" href="{{ route('admin.author.index') }}"><i class="icon fa fa-circle-o"></i> 作者列表</a></li>
+                <li><a class="treeview-item" data-nav="admin.image.index" href="{{ route('admin.image.index') }}"><i class="icon fa fa-circle-o"></i> 封面图片</a></li>
                 {{--<li><a class="treeview-item" data-nav="admin.goods_export.index" href="{{ route('admin.goods_export.index') }}"><i class="icon fa fa-circle-o"></i> 出库列表</a></li>--}}
                 {{--<li><a class="treeview-item" data-nav="admin.category.index" href="{{ route('admin.category.index') }}"><i class="icon fa fa-circle-o"></i> 分类管理</a></li>--}}
             </ul>
@@ -68,21 +69,6 @@
             </a>
             <ul class="treeview-menu">
                 <li><a class="treeview-item" data-nav="admin.check_info.index" href="{{ route('admin.check_info.index') }}"><i class="icon fa fa-circle-o"></i> 异常列表</a></li>
-                {{--<li><a class="treeview-item" data-nav="admin.goods_import.index" href="{{ route('admin.goods_import.index') }}"><i class="icon fa fa-circle-o"></i> 进库列表</a></li>--}}
-                {{--<li><a class="treeview-item" data-nav="admin.goods_export.index" href="{{ route('admin.goods_export.index') }}"><i class="icon fa fa-circle-o"></i> 出库列表</a></li>--}}
-                {{--<li><a class="treeview-item" data-nav="admin.category.index" href="{{ route('admin.category.index') }}"><i class="icon fa fa-circle-o"></i> 分类管理</a></li>--}}
-            </ul>
-        </li>
-
-        <li class="treeview">
-            <a class="app-menu__item" href="#" data-toggle="treeview">
-                <i class="app-menu__icon fa fa-laptop"></i>
-                <span class="app-menu__label">工具管理</span>
-                <i class="treeview-indicator fa fa-angle-right"></i>
-            </a>
-            <ul class="treeview-menu">
-                <li><a class="treeview-item" data-nav="admin.image.index" href="{{ route('admin.image.index') }}"><i class="icon fa fa-circle-o"></i> 封面图片</a></li>
-                {{--<li><a class="treeview-item" data-nav="admin.category.index" href="{{ route('admin.category.index') }}"><i class="icon fa fa-circle-o"></i> 小说分类列表</a></li>--}}
                 {{--<li><a class="treeview-item" data-nav="admin.goods_import.index" href="{{ route('admin.goods_import.index') }}"><i class="icon fa fa-circle-o"></i> 进库列表</a></li>--}}
                 {{--<li><a class="treeview-item" data-nav="admin.goods_export.index" href="{{ route('admin.goods_export.index') }}"><i class="icon fa fa-circle-o"></i> 出库列表</a></li>--}}
                 {{--<li><a class="treeview-item" data-nav="admin.category.index" href="{{ route('admin.category.index') }}"><i class="icon fa fa-circle-o"></i> 分类管理</a></li>--}}
